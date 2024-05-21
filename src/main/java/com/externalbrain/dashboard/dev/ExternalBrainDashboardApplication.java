5998d52e1e3e848c6527619a3af200dc68a703e2
Emanuel Sotomayor <emanuel.sotomayor.sec@gmail.com>
2024-05-21 13:36:16 -0300

    Initial Project

---
+++ src/main/java/com/externalbrain/dashboard/dev/ExternalBrainDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.externalbrain.dashboard.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalBrainDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalBrainDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/externalbrain/dashboard/dev/ExternalBrainDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.externalbrain.dashboard.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalBrainDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
